5c22d84561587131b24b88decd3047eaf0dba644
João Marcos <jmarcossferreira@hotmail.com>
2023-12-27 13:20:39 -0300

    feat: add new variant to button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Button } from '@/components/ui/button';
 
 function App() {
   return (
-    <main className="h-screen bg-slate-800 text-white p-2">
+    <main className="h-screen bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-white p-2">
       <div className="flex flex-col justify-center items-center h-full gap-4">
         <h1 className="text-xl font-semibold underline">Hello from shadcn</h1>
 
-        <Button size="lg" variant="destructive">
+        <Button size="lg" variant="success">
           I'm a button
         </Button>
       </div>
